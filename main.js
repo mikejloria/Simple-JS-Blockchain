a13cc43d01c21be1428b9581a20cf38b4d405cbb
MJ <mikejloria@gmail.com>
2023-09-25 21:27:03 -0700

    proof of work

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ class Block {
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
-    this.hash = this.calculateHash;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash() {
@@ -14,14 +15,26 @@ class Block {
       this.index +
         this.previousHash +
         this.timestamp +
-        JSON.stringify(this.data)
+        JSON.stringify(this.data) +
+        this.nonce
     ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block Mined: " + this.hash);
   }
 }
 
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 5;
   }
 
   createGenesisBlock() {
@@ -34,7 +47,8 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    //newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 
@@ -56,11 +70,13 @@ class Blockchain {
 }
 
 let jsCoin = new Blockchain();
+console.log("Mining Block 1... ");
 jsCoin.addBlock(new Block(1, "9/11/2023", { amount: 1234 }));
+console.log("Mining Block 2... ");
 jsCoin.addBlock(new Block(2, "9/11/2023", { amount: 4321 }));
 
 // jsCoin.chain[1].data = { amount: 10000000000 };
 // jsCoin.chain[1].hash = jsCoin.chain[1].calculateHash();
 // console.log("Is Blockchain Valid? " + jsCoin.isChainValid());
 
-console.log(JSON.stringify(jsCoin, null, 4));
+//console.log(JSON.stringify(jsCoin, null, 4));
